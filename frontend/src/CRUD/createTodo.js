5d71ae00b6c1ea27cbfcb0b040f2888ae72e9750
Wizock <silent.death3500@gmail.com>
2022-03-31 06:21:47 +1000

    beginings of the ui

--- frontend/src/CRUD/createTodo.js
+++ frontend/src/CRUD/createTodo.js
@@ -2,39 +2,57 @@ import React, {forwardRef, useState, useEffect} from "react";
 import {useHistory} from "react-router-dom";
 import Navbar from "./navbarComponent";
 
-import "react-datepicker/dist/react-datepicker.css";
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Stack from '@mui/material/Stack';
+
+import TimePicker from '@mui/lab/TimePicker';
 const axios = require("axios");
 
 function CreateTodo() {
+	//get todays date
+
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [priority, setPriority] = useState("");
+	const [error, setError] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
 	const token = localStorage.getItem("token");
 	const history = useHistory();
+	const [startingDateValue, setStartingDateValue] = useState(new Date());
+	const [dueDateValue, setDueDateValue]           = useState(new Date());
+	const [dueTimeValue, setDueTimeValue]           = useState(0);
+
+
 
 	return (
 		<div>
 			{token && token !== "" && token !== undefined ? (
 				<div>
 					<Navbar />
-					<div className="flex justify-center">
-						<div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-							<h5 className="text-gray-900 text-xl leading-tight font-medium mb-2"> Card title </h5>
+
+					
+					<div className=" flex content-center items-center justify-center h-full flex justify-center ">
+						<div className=" block p-6 rounded-lg shadow-lg bg-white max-w-sm w-full lg:w-4/10 px-4 ">
+							<h5 className="text-gray-900 text-xl leading-tight font-medium mb-2 w-full lg:w-4/10 px-4"> Card title </h5>
 							<div className="text-gray-700 text-base mb-4">
 								<div className="flex justify-center">
-									<div className="mb-3 xl:w-96">
-										<label htmlFor="exampleText0" className="form-label inline-block mb-2 text-gray-700"> Text input </label>
-										<input type="text" className=" form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"id="exampleText0" placeholder="Text input"></input>
-										<div className="flex items-center justify-center">
-											<div className="datepicker relative form-floating mb-3 xl:w-96" data-mdb-toggle-button="false">
-												<input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Select a date" data-mdb-toggle="datepicker"/>
-												<label htmlFor="floatingInput" className="text-gray-700"> Select a date </label>
-											</div>
-										</div>
-										<div className="flex items-center justify-center">
-											<div className="datepicker relative form-floating mb-3 xl:w-96" data-mdb-toggle-button="false">
-											</div>
-										</div>
-										<div className="flex items-center justify-center">
-											<div className="datepicker relative form-floating mb-3 xl:w-96" data-mdb-toggle-button="false">
-											</div>
+									<div className="w-full lg:w-4/10 px-4">
+										<div className="mt-3 flex items-center justify-center">
+										<Stack spacing={3}>
+											<TextField id="outlined-basic" label="Task Title" variant="outlined" />
+
+											<LocalizationProvider dateAdapter={AdapterDateFns}>
+											<DatePicker label="Initial Date" value={startingDateValue} onChange={(newValue) => {setStartingDateValue(newValue);}}renderInput={(params) => <TextField {...params} />}/>
+											<DatePicker label="Due Date" value={dueDateValue} onChange={(newValue) => {setDueDateValue(newValue);}}renderInput={(params) => <TextField {...params} />}/>
+											<TimePicker label="Time Due" value={dueTimeValue} onChange={(newValue) => { setDueTimeValue(newValue); }} renderInput={(params) => <TextField {...params} />}/>
+											</LocalizationProvider>
+										</Stack>
 										</div>
 									</div>
 								</div>
