6a9a4076a0a161c1ebc1fc13bcc26e74d65595e5
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-16 21:31:10 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import {LoginPage} from './AUTH/login'
-import {RegisterPage} from './AUTH/register'
-import {GetUser} from './CRUD/getUsersComponent'
-import {CreateTodo} from './CRUD/createTodo'
-import {GetUserTodos} from './CRUD/showAll'
+import { LoginPage } from "./AUTH/login";
+import { RegisterPage } from "./AUTH/register";
+import { GetUser } from "./CRUD/getUsersComponent";
+import { CreateTodo } from "./CRUD/createTodo";
+import { GetUserTodos } from "./CRUD/showAll";
 
-
-function Homepage (){
-    return (
-        <div>
-            <Router>
-                <Link to="/todo"></Link>
-                <Link to="/login"></Link>
-                <Link to="/register"></Link>
-                <Link to="/users"></Link>
-                <Link to="/crudRoute"></Link>
-                <Switch>
-                    <Route exact path="/login" component={LoginPage}/>
-                    <Route exact path="/register" component={RegisterPage}/>
-                    <Route exact path="/users" component={GetUser}/>
-                    <Route exact path="/crudRoute" component={CreateTodo}/>
-                    <Route exact path="/todo" component={GetUserTodos}/>
-                </Switch>
-            </Router>
-        </div>
-    );
+function Homepage() {
+  return (
+    <div>
+      <Router>
+        <Link to="/todo"></Link>
+        <Link to="/login"></Link>
+        <Link to="/register"></Link>
+        <Link to="/users"></Link>
+        <Link to="/crudRoute"></Link>
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/users" component={GetUser} />
+          <Route exact path="/crudRoute" component={CreateTodo} />
+          <Route exact path="/todo" component={GetUserTodos} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 function App() {
-    return <Homepage/>
+  return <Homepage />;
 }
 
 export default App;
-
