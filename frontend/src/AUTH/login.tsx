a69fa95647f709bff9b45a4b74e213272c5251c1
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-06-13 14:23:03 +1000

    Update login.tsx

--- frontend/src/AUTH/login.tsx
+++ frontend/src/AUTH/login.tsx
@@ -4,218 +4,6 @@ import axios from "axios";
 import { GetToken } from "../tokenFetcher";
 /* eslint @typescript-eslint/no-var-requires: "off" */
 function Tlogin() {
-<<<<<<< HEAD
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [token, setToken] = useState("");
-	const history = useHistory();
-
-	const handleSubmit = () => {
-		axios({
-			method: "POST",
-			url: "/auth/login",
-			data: {
-				username: username,
-				password: password,
-			},
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((response) => {
-				console.log(response);
-				setToken(response.data);
-				localStorage.setItem("token", token);
-				history.push("/todo");
-			})
-			.catch((error) => {
-				console.log(error);
-			}
-		);
-	};
-	return (
-		<div>
-			{GetToken() === localStorage.getItem("token") ? (
-				<div>
-					<h1>You are logged in</h1>
-					<h2>{token}</h2>
-					<button onClick={() => history.push("/")}>
-						Home
-					</button>
-				</div>
-			) : (
-				<main>
-					<section className="absolute w-full h-full">
-						<div
-							className="absolute top-0 w-full h-full bg-gray-900"
-							style={{
-								backgroundImage: `url(${require("../assets/auth/register_bg_2.png")})`,
-								backgroundSize: "100%",
-								backgroundRepeat: "no-repeat",
-							}}></div>
-						<div className="container mx-auto px-4 h-full">
-							<div className="flex content-center items-center justify-center h-full">
-								<div className="w-full lg:w-4/10 px-4">
-									<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-										<div className="rounded-t mb-0 px-6 py-6">
-											<div className="text-center mb-3">
-												<h6 className="text-gray-600 text-sm font-bold">
-													Sign in with
-												</h6>
-											</div>
-											<div className="btn-wrapper text-center">
-												<button
-													className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-													type="button"
-													style={{
-														transition:
-															"all .15s ease",
-													}}>
-													<img
-														alt="..."
-														className="w-5 mr-1"
-														src={
-															require("../assets/auth/github.svg")
-																.default
-														}
-													/>
-													Github
-												</button>
-												<button
-													className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-													type="button"
-													style={{
-														transition:
-															"all .15s ease",
-													}}>
-													<img
-														alt="..."
-														className="w-5 mr-1"
-														src={
-															require("../assets/auth/google.svg")
-																.default
-														}
-													/>
-													Google
-												</button>
-											</div>
-											<hr className="mt-6 border-b-1 border-gray-400" />
-										</div>
-										<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-											<div className="text-gray-500 text-center mb-3 font-bold">
-												<small>
-													Or sign in with credentials
-												</small>
-											</div>
-											<form
-												onSubmit={handleSubmit}
-												id="form">
-												<div className="relative w-full mb-3">
-													<label
-														className="block uppercase text-gray-700 text-xs font-bold mb-2"
-														htmlFor="grid-password">
-														Username
-													</label>
-													<input
-														type="text"
-														id="username"
-														className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-														placeholder="Username"
-														style={{
-															transition:
-																"all .15s ease",
-														}}
-													/>
-												</div>
-												<div className="relative w-full mb-3">
-													<label
-														className="block uppercase text-gray-700 text-xs font-bold mb-2"
-														htmlFor="grid-password">
-														Password
-													</label>
-													<input
-														type="password"
-														className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-														placeholder="Password"
-														id="password"
-														style={{
-															transition:
-																"all .15s ease",
-														}}
-													/>
-												</div>
-												<div>
-													<label className="inline-flex items-center cursor-pointer">
-														<input
-															id="customCheckLogin"
-															type="checkbox"
-															className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
-															style={{
-																transition:
-																	"all .15s ease",
-															}}
-														/>
-														<span className="ml-2 text-sm font-semibold text-gray-700">
-															Remember me
-														</span>
-													</label>
-												</div>
-												<div className="text-center mt-6">
-													<button
-														className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-														style={{
-															transition:
-																"all .15s ease",
-														}}
-														type="submit"
-														onClick={() => {
-															setUsername(
-																(document.getElementById("username") as HTMLInputElement).value
-															);
-															setPassword(
-																(document.getElementById("password") as HTMLInputElement).value
-															);
-														}}>
-														Sign In
-													</button>
-													<small>
-													Dont Have an Account? please <a href='/register'>Register</a>
-												</small>
-												</div>
-											</form>
-										</div>
-									</div>
-									<div className="flex flex-wrap mt-6">
-										<div className="w-1/2">
-											<a
-												href="#pablo"
-												onClick={(e) =>
-													e.preventDefault()
-												}
-												className="text-gray-300">
-												<small>Forgot password?</small>
-											</a>
-										</div>
-										<div className="w-1/2 text-right">
-											<a
-												href="#pablo"
-												onClick={(e) =>
-													e.preventDefault()
-												}
-												className="text-gray-300">
-											
-											</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-				</main>
-			)}
-		</div>
-	);
-=======
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
@@ -260,7 +48,8 @@ function Tlogin() {
                 backgroundImage: `url(${require("../assets/auth/register_bg_2.png")})`,
                 backgroundSize: "100%",
                 backgroundRepeat: "no-repeat",
-              }}></div>
+              }}
+            ></div>
             <div className="container mx-auto px-4 h-full">
               <div className="flex content-center items-center justify-center h-full">
                 <div className="w-full lg:w-4/10 px-4">
@@ -277,7 +66,8 @@ function Tlogin() {
                           type="button"
                           style={{
                             transition: "all .15s ease",
-                          }}>
+                          }}
+                        >
                           <img
                             alt="..."
                             className="w-5 mr-1"
@@ -290,7 +80,8 @@ function Tlogin() {
                           type="button"
                           style={{
                             transition: "all .15s ease",
-                          }}>
+                          }}
+                        >
                           <img
                             alt="..."
                             className="w-5 mr-1"
@@ -309,7 +100,8 @@ function Tlogin() {
                         <div className="relative w-full mb-3">
                           <label
                             className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-password">
+                            htmlFor="grid-password"
+                          >
                             Username
                           </label>
                           <input
@@ -325,7 +117,8 @@ function Tlogin() {
                         <div className="relative w-full mb-3">
                           <label
                             className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="grid-password">
+                            htmlFor="grid-password"
+                          >
                             Password
                           </label>
                           <input
@@ -375,7 +168,8 @@ function Tlogin() {
                                   ) as HTMLInputElement
                                 ).value
                               );
-                            }}>
+                            }}
+                          >
                             Sign In
                           </button>
                           <small>
@@ -391,7 +185,8 @@ function Tlogin() {
                       <a
                         href="#pablo"
                         onClick={(e) => e.preventDefault()}
-                        className="text-gray-300">
+                        className="text-gray-300"
+                      >
                         <small>Forgot password?</small>
                       </a>
                     </div>
@@ -399,7 +194,8 @@ function Tlogin() {
                       <a
                         href="#pablo"
                         onClick={(e) => e.preventDefault()}
-                        className="text-gray-300"></a>
+                        className="text-gray-300"
+                      ></a>
                     </div>
                   </div>
                 </div>
@@ -410,7 +206,7 @@ function Tlogin() {
       )}
     </div>
   );
->>>>>>> 6a9a4076a0a161c1ebc1fc13bcc26e74d65595e5
 }
 
 export default Tlogin;
+
